b64886dba60882df949392820ad758f9d8ed71fe
Rfratini <rafael.corso@sptech.school>
2025-05-15 19:30:00 -0300

    adicionei script sql e coloquei nos inputs limites de caracteres de acordo com o banco de dados

---
+++ Banco de dados/ScriptGow.sql
@@ -0,0 +1,12 @@
+create database projetoGow;
+use projetoGow;
+
+create table usuario 
+(
+idusuario int primary key auto_increment,
+username varchar(20) not null,
+email varchar(45) not null unique,
+senha varchar(20) not null,
+genero varchar(9) not null
+);
+
