79c216f1c3736869eb2b437a1465b3c91f3a2805
Rfratini <rafael.corso@sptech.school>
2025-05-18 20:05:22 -0300

    função de cadastro e login funcionando dentro da API

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -1,6 +1,4 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
-
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
@@ -17,25 +15,19 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    if (resultadoAutenticar.length == 1) {
+                   if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        console.log("Usuário encontrado: ", resultadoAutenticar[0]);
+                        console.log("Autenticação realizada com sucesso!");
+                        res.json({
+                            id: resultadoAutenticar[0].idusuario,
+                            email: resultadoAutenticar[0].email,
+                            username: resultadoAutenticar[0].username,
+                            senha: resultadoAutenticar[0].senha,
+                            genero: resultadoAutenticar[0].genero
+                        });
                     } else if (resultadoAutenticar.length == 0) {
-                        alert("Email e/ou senha inválido(s)");
+                        res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }
@@ -53,20 +45,20 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var username = req.body.nomeServer;
+    var username = req.body.usernameServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
     var genero = req.body.generoServer;
 
     // Faça as validações dos valores
     if (username == undefined) {
-        alert("Seu nome está undefined!");
+        console.log("Seu nome está undefined!");
     } else if (email == undefined) {
-       alert("Seu email está undefined!");
+       console.log("Seu email está undefined!");
     } else if (senha == undefined) {
-        alert("Sua senha está undefined!");
+        console.log("Sua senha está undefined!");
     } else if (genero == undefined) {
-       alert("Sua Seu gênero  está undefined!");
+       console.log("Sua Seu gênero está undefined!");
     }  else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
