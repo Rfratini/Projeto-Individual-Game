d5c32a39de9d8afafddc54ea5ebd80cbe146f48f
Rfratini <rafael.corso@sptech.school>
2025-05-29 14:49:51 -0300

    alteração no script do BD

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,62 +1,40 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database if not exists projetoGow;
+use projetoGow;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table if not exists usuario 
+(
+idusuario int primary key auto_increment,
+username varchar(20) not null,
+email varchar(45) not null unique,
+senha varchar(20) not null,
+genero varchar(20) not null
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table if not exists quiz 
+(
+idpergunta int primary key auto_increment, 
+Resposta char(1)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table if not exists tentativa 
+(
+Fkusuario int,
+Fkquiz int,
+primary key (Fkusuario, Fkquiz),
+constraint usuarioFK foreign key (Fkusuario) references usuario(idusuario),
+constraint QuizFK foreign key (Fkquiz) references quiz(idpergunta),
+Pontuacao decimal(4,2),
+Respusuario char(1)
 );
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+-- Falta alterar ainda
+insert into quiz values
+(1, 'C'),
+(2, 'A'),
+(3, 'B'),
+(4, 'C'),
+(5, 'C'),
+(6, 'C'),
+(7, 'C'),
+(8, 'C'),
+(9, 'C'),
+(10, 'C');
