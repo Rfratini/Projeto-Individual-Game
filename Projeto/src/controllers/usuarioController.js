bcf2f1adeb86e165ce0de8595d6b87f3978c1705
Rfratini <rafael.corso@sptech.school>
2025-05-30 15:36:05 -0300

    começei a fazer as rotas para inserir no bd

--- Projeto/src/controllers/usuarioController.js
+++ Projeto/src/controllers/usuarioController.js
@@ -79,8 +79,46 @@ function cadastrar(req, res) {
             );
     }
 }
+/*
+function finaçiar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var Fkquiz = req.body.usernameServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    var genero = req.body.generoServer;
 
+    // Faça as validações dos valores
+    if (username == undefined) {
+        console.log("Seu nome está undefined!");
+    } else if (email == undefined) {
+       console.log("Seu email está undefined!");
+    } else if (senha == undefined) {
+        console.log("Sua senha está undefined!");
+    } else if (genero == undefined) {
+       console.log("Sua Seu gênero está undefined!");
+    }  else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(username, email, senha, genero)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+    */
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    quiz
 }
--- Projeto/src/models/usuarioModel.js
+++ Projeto/src/models/usuarioModel.js
@@ -22,7 +22,20 @@ function cadastrar(username, email, senha, genero) {
     return database.executar(instrucaoSql);
 }
 
+function quiz(Fkusuario, Fkquiz, Pontuacao, Respusuario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", username, email, senha, genero);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (Fkusuario, Fkquiz, Pontuacao, Respusuario) VALUES ('${Fkusuario}', '${Fkusuario}', '${Pontuacao}', '${Respusuario}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    quiz
 };
